4accdb321ae2b90ad224553b9dcc72841beec001
Vlad Lazar <vladlazar94@outlook.com>
2021-04-02 14:31:05 +0300

    Added a production webpack config.

---
+++ webpack.prod.ts
@@ -0,0 +1,54 @@
+import path from "path";
+import Webpack from "webpack";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+
+const config: Webpack.Configuration = {
+    mode: "production",
+    entry: path.resolve(__dirname, "src", "main.ts"),
+    output: {
+        filename: "[name].bundle.js",
+        path: path.resolve(__dirname, "dist"),
+    },
+    resolve: {
+        extensions: [".ts", ".tsx", ".js", "jsx"],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(t|j)sx?$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: "@linaria/webpack-loader",
+                        options: { sourceMap: false },
+                    },
+                    { loader: "ts-loader" },
+                ],
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                    },
+                    {
+                        loader: "css-loader",
+                        options: { sourceMap: false },
+                    },
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, "static", "index.html"),
+            favicon: path.resolve(__dirname, "static", "hammer.png"),
+        }),
+        new MiniCssExtractPlugin({
+            filename: "[name].[contenthash].css",
+        }),
+    ],
+};
+
+export default config;
